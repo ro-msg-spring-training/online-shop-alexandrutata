d434729c17e68a5ce78d600ed605e08bc462e564
Alexandru Tata <alexandru.tata@msg.group>
2022-03-25 14:13:53 +0200

    .

---
+++ shop/src/main/java/ro/msg/learning/shop/ApplicationSecurity.java
@@ -0,0 +1,21 @@
+package ro.msg.learning.shop;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+    // @Override
+    // public void configure(WebSecurity web) throws Exception {
+    //     web.ignoring().antMatchers("/**");
+    // }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2/*").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+}
