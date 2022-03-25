07d6b0f38c91fcfdf581cfdead52c2bbdd369ff3
Alexandru Tata <alexandru.tata@msg.group>
2022-03-25 14:38:22 +0200

    .

--- shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -1,13 +1,22 @@
 package ro.msg.learning.shop;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class ShopApplication {
 
 	public static void main(String[] args) {
+		log.info("------- > Starting application at {}", LocalDateTime.now());
+
 		SpringApplication.run(ShopApplication.class, args);
+		
+		log.info("------- > App started at {}...", LocalDateTime.now());
 	}
 
 }
