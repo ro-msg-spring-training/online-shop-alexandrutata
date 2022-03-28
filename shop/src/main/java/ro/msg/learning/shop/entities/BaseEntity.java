0005ac914fd31a703818542fce92131b9ea689c6
Alexandru Tata <alexandru.tata@msg.group>
2022-03-28 17:37:30 +0300

    .

--- shop/src/main/java/ro/msg/learning/shop/entities/BaseEntity.java
+++ shop/src/main/java/ro/msg/learning/shop/entities/BaseEntity.java
@@ -6,12 +6,14 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 @MappedSuperclass
 @Getter @Setter
 @SuperBuilder
+@NoArgsConstructor
 public class BaseEntity {
     @Id 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- shop/src/main/java/ro/msg/learning/shop/entities/Supplier.java
+++ shop/src/main/java/ro/msg/learning/shop/entities/Supplier.java
@@ -5,11 +5,13 @@ import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Entity
 @Data
 @SuperBuilder
+@NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Table(name = "supplier")
 public class Supplier extends BaseEntity {
