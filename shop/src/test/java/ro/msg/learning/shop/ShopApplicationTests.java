d3bf870a6193e500d19680f994747a94ad72548d
Alexandru Tata <alexandru.tata@msg.group>
2022-03-28 17:53:44 +0300

    .

--- shop/src/test/java/ro/msg/learning/shop/ShopApplicationTests.java
+++ shop/src/test/java/ro/msg/learning/shop/ShopApplicationTests.java
@@ -1,17 +1,16 @@
 package ro.msg.learning.shop;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.util.Assert;
 
-import lombok.extern.slf4j.Slf4j;
 import ro.msg.learning.shop.entities.Supplier;
 import ro.msg.learning.shop.repositories.SupplierRepository;
 
 @SpringBootTest
-@Slf4j
 @ActiveProfiles("test")
 class ShopApplicationTests {
 
@@ -24,13 +23,11 @@ class ShopApplicationTests {
 				.id(1)
 				.name("my first supplier")
 				.build();
-		try {
-			supplierRepository.save(supplier);
-		} catch (Exception e) {
-			log.error("------------------------> Error: ", e);
-		}
-		Assert.isTrue(true, "WORKS!!!");
-		//supplier = supplierRepository.getById(1);
+	
+		supplierRepository.save(supplier);
+
+		var name = supplierRepository.getById(1).getName();
+		assertEquals(supplier.getName(), name);
 	}
 
 }
