b8fd2515f509501a5eb0f19810dd1f44af4d54a7
Alexandru Tata <alexandru.tata@msg.group>
2022-03-28 23:06:54 +0300

    .

--- shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
+++ shop/src/main/java/ro/msg/learning/shop/ShopApplication.java
@@ -2,13 +2,10 @@ package ro.msg.learning.shop;
 
 import java.time.LocalDateTime;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
-import ro.msg.learning.shop.model.Supplier;
-import ro.msg.learning.shop.repository.SupplierRepository;
 
 @Slf4j
 @SpringBootApplication
@@ -20,28 +17,10 @@ public class ShopApplication {
 		SpringApplication.run(ShopApplication.class, args);
 		
 		log.info("------- > App started at {}...", LocalDateTime.now());
-
-		// var testRepo = (new ShopApplication()).new TestRepo();
-		// testRepo.test();
 	}
 
-	public class TestRepo{
-	
-		@Autowired
-		private SupplierRepository supplierRepository;
-	
-		public void test(){
-			var supplier = Supplier.builder()
-				.id(24)
-				.name("my first supplier")
-				.build();
-	
-			supplierRepository.save(supplier);
-	
-			var supplier2 = supplierRepository.getById(24);
-			int id = supplier2.getId();
-			var name = supplier2.getName();
-		}
+	public ShopApplication(){
+		var testRepo = new TestRepository();
+		testRepo.test();
 	}
-	
 }
