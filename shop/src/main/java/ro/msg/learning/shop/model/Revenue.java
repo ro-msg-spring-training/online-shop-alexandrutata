647ed69a1ea66d7f8c325c893503f846cffc1585
Alexandru Tata <alexandru.tata@msg.group>
2022-03-29 10:48:57 +0300

    .

--- shop/src/main/java/ro/msg/learning/shop/model/Revenue.java
+++ shop/src/main/java/ro/msg/learning/shop/model/Revenue.java
@@ -10,7 +10,6 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.AccessLevel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
