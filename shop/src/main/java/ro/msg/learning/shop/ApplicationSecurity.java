e17f4017e851515f1fd25e762d663c239b92b02a
Alexandru Tata <alexandru.tata@msg.group>
2022-04-04 21:44:18 +0300

    .

--- shop/src/main/java/ro/msg/learning/shop/ApplicationSecurity.java
+++ shop/src/main/java/ro/msg/learning/shop/ApplicationSecurity.java
@@ -1,20 +1,43 @@
 package ro.msg.learning.shop;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
     // @Override
     // public void configure(WebSecurity web) throws Exception {
-    //     web.ignoring().antMatchers("/**");
+    // web.ignoring().antMatchers("/**");
+    // }
+
+    // @Override
+    // public void configure(WebSecurity web) throws Exception {
+    //     web.ignoring()
+    //             // Spring Security should completely ignore URLs starting with /resources/
+    //             .antMatchers("/resources/**");
     // }
 
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/h2/*").permitAll();
-        http.csrf().disable();
-        http.headers().frameOptions().disable();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2/**").permitAll()
+                .anyRequest().hasRole("USER")
+            .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // enable in memory based authentication with a user named "user" and "admin"
+        auth.inMemoryAuthentication()
+            .withUser("user").password("{noop}password").roles("USER")
+                .and()
+            .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+    }
+
 }
